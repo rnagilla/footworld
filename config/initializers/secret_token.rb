48e2c6a4b8ae4935907709a6df26dee0f133de56
RakeshReddyNagilla <nagillarakeshreddy@gmail.com>
2011-09-30 17:12:04 -0500

    rails default files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footworlds::Application.config.secret_token = 'b10171284a64c2f89abf2f17ffd16e0badb4062876fdd6f33f313578459ce877758dc9a2d2a9cbe4436c85a2e790c3e3ec39d0c2a34b629aa4d5d2ee4df6eab4'
